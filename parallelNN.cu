30a0fe51577424eb405ca8dee9a0b545a6bd0267
Johan Ferreras <ferrerasjohan@gmail.com>
2019-05-21 23:38:46 -0400

    changes

--- parallelNN.cu
+++ parallelNN.cu
@@ -263,6 +263,8 @@ int main(int argc, char** argv){
                         }
                     }
                 }
+                std::cout << "1" << std::endl;
+                std::cout << input_layer_w[0][0][0] << std::endl;
 
                 for(int k = 0; k < (int) NUM_NEURONS/EPOCH_SIZE; k++){
                     for(int n = 0; n < NUM_NEURONS; n++){
@@ -309,13 +311,14 @@ int main(int argc, char** argv){
                     for(int r = 0; r < ROWS; r++){
                         for(int c = 0; c < COLS; c++){
                             input_layer_w[n][r][c] -=  input_layer_deriv[n][r][c] * learning_rate;
-                            std::cout << input_layer_w[n][r][c] << std::endl;
                             input_layer_deriv[n][r][c] = 0;
                         }
                     }
                     first_layer_bias[n] -= first_layer_bias_deriv[n] * learning_rate;
                     first_layer_bias_deriv[n] = 0;
                 }
+                std::cout << "2" << std::endl;
+                std::cout << input_layer_w[0][0][0] << std::endl;
 
                 for(int k = 0; k < (int) NUM_NEURONS/EPOCH_SIZE; k++){
                     for(int n = 0; n < NUM_NEURONS; n++){
