030cd37c3cb5b649328322926184b58cc5de7f8c
Johan Ferreras <ferrerasjohan@gmail.com>
2019-05-21 23:32:50 -0400

    changes

--- parallelNN.cu
+++ parallelNN.cu
@@ -129,7 +129,7 @@ void generateWeights(float*** (&ilw), float*** (&ild), float** (&fclw), float** 
             for(int c = 0; c < COLS; c++){
                 //normal_distribution represents unbownded distribution, divide by sqrt(N)
                 ilw[n][r][c] = distribution(generator) / sqrt(NUM_NEURONS);
-                std::cout << distribution(generator) / sqrt(NUM_NEURONS) << std::endl;
+                //std::cout << distribution(generator) / sqrt(NUM_NEURONS) << std::endl;
                 //initially weights are 0
                 ild[n][r][c] = 0;
             }
